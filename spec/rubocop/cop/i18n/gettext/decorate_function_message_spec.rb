fdc2ea41e45e9f0ca418891597342359972774f2
Brandon High <brandon.high@puppet.com>
2017-08-14 15:24:52 +0100

    Update specs to cover broken raise
    This commit adds tests that are flagged as broken so we can track
    when the raise function is fixed.

--- spec/rubocop/cop/i18n/gettext/decorate_function_message_spec.rb
+++ spec/rubocop/cop/i18n/gettext/decorate_function_message_spec.rb
@@ -6,8 +6,9 @@ describe RuboCop::Cop::I18n::GetText::DecorateFunctionMessage do
   subject(:cop) { described_class.new(config) }
 
   it_behaves_like 'accepts', 'fail _("a string")'
+  it_behaves_like 'accepts', 'raise _("a string")'
 
-  context 'undecorated string' do
+  context 'undecorated fail string' do
     let(:source) { 'fail "a string"' }
 
     it 'rejects' do
@@ -17,7 +18,17 @@ describe RuboCop::Cop::I18n::GetText::DecorateFunctionMessage do
     end
   end
 
-  context 'multiline string' do
+  context 'undecorated raise string' do
+    let(:source) { 'raise "a string"' }
+
+    it 'rejects', broken: true do
+      investigate(cop, source)
+      expect(cop.offenses.size).to eq(1)
+      expect(cop.offenses[0].message).to match(/should have a decorator around the message/)
+    end
+  end
+
+  context 'multiline fail string' do
     let(:source) { <<-RUBY
 fail <<-STR
   this
@@ -35,7 +46,25 @@ STR
     end
   end
 
-  context 'concatenated string' do
+  context 'multiline raise string' do
+    let(:source) { <<-RUBY
+raise <<-STR
+  this
+  is
+  a
+  string
+STR
+    RUBY
+    }
+
+    it 'rejects', broken: true do
+      investigate(cop, source)
+      expect(cop.offenses.size).to eq(1)
+      expect(cop.offenses[0].message).to match(/should not use a multi-line string/)
+    end
+  end
+
+  context 'concatenated fail string' do
     let(:source) { <<-RUBY
 fail "this" + "string" + "is" + "concatenated"
     RUBY
@@ -48,10 +77,37 @@ fail "this" + "string" + "is" + "concatenated"
     end
   end
 
-  context 'interpolated string' do
+  context 'concatenated raise string' do
+    let(:source) { <<-RUBY
+raise "this" + "string" + "is" + "concatenated"
+    RUBY
+    }
+
+    it 'rejects', broken: true do
+      investigate(cop, source)
+      expect(cop.offenses.size).to eq(1)
+      expect(cop.offenses[0].message).to match(/should not use a concatenated string/)
+    end
+  end
+
+  context 'interpolated fail string' do
     let(:source) { <<-RUBY
 var = "foo"
 fail "this string has a \#{var}"
+    RUBY
+    }
+
+    it 'rejects' do
+      investigate(cop, source)
+      expect(cop.offenses.size).to eq(1)
+      expect(cop.offenses[0].message).to match(/interpolation is a sin/)
+    end
+  end
+
+  context 'interpolated raise string', broken: true do
+    let(:source) { <<-RUBY
+var = "foo"
+raise "this string has a \#{var}"
     RUBY
     }
 
