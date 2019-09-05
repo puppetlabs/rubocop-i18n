8ecefeda171d7b36b115b089f540c662a0386a76
David Jones <unixmonkey1@gmail.com>
2019-09-04 21:46:09 -0400

    Port over Rubocop fixes in new files to existing files.

--- lib/rubocop/cop/i18n/gettext/decorate_string_formatting_using_interpolation.rb
+++ lib/rubocop/cop/i18n/gettext/decorate_string_formatting_using_interpolation.rb
@@ -29,10 +29,10 @@ module RuboCop
 
             method_name = node.method_name
             arg_nodes = node.arguments
-            if !arg_nodes.empty? && contains_string_formatting_with_interpolation?(arg_nodes)
-              message_section = arg_nodes[0]
-              add_offense(message_section, message: "'#{method_name}' function, message string should not contain \#{} formatting")
-            end
+            return unless !arg_nodes.empty? && contains_string_formatting_with_interpolation?(arg_nodes)
+
+            message_section = arg_nodes[0]
+            add_offense(message_section, message: "'#{method_name}' function, message string should not contain \#{} formatting")
           end
 
           private
@@ -42,19 +42,11 @@ module RuboCop
           end
 
           def contains_string_formatting_with_interpolation?(node)
-            if node.is_a?(Array)
-              return node.any? { |n| contains_string_formatting_with_interpolation?(n) }
-            end
+            return node.any? { |n| contains_string_formatting_with_interpolation?(n) } if node.is_a?(Array)
 
-            if node.respond_to?(:type)
-              if node.str_type? || node.dstr_type?
-                return string_contains_interpolation_format?(node.source)
-              end
-            end
+            return string_contains_interpolation_format?(node.source) if node.respond_to?(:type) && (node.str_type? || node.dstr_type?)
 
-            if node.respond_to?(:children)
-              return node.children.any? { |child| contains_string_formatting_with_interpolation?(child) }
-            end
+            return node.children.any? { |child| contains_string_formatting_with_interpolation?(child) } if node.respond_to?(:children)
 
             false
           end
--- spec/rubocop/cop/i18n/gettext/decorate_string_formatting_using_interpolation_spec.rb
+++ spec/rubocop/cop/i18n/gettext/decorate_string_formatting_using_interpolation_spec.rb
@@ -10,6 +10,8 @@ describe RuboCop::Cop::I18n::GetText::DecorateStringFormattingUsingInterpolation
   end
 
   RuboCop::Cop::I18n::GetText.supported_decorators.each do |decorator|
+    error_message = "function, message string should not contain \#{} formatting"
+
     context "#{decorator} decoration not used" do
       it_behaves_like 'a_no_cop_required', 'thing("a \#{true} that is not decorated")'
     end
@@ -22,20 +24,20 @@ describe RuboCop::Cop::I18n::GetText::DecorateStringFormattingUsingInterpolation
     end
 
     context "#{decorator} decoration with formatting" do
-      it_behaves_like 'a_detecting_cop', "#{decorator}(\"a \#{true}\")", '_', 'function, message string should not contain #{} formatting'
+      it_behaves_like 'a_detecting_cop', "#{decorator}(\"a \#{true}\")", '_', error_message
     end
 
     context "#{decorator} decoration with multiple interpolations" do
-      it_behaves_like 'a_detecting_cop', "#{decorator}(\"a \#{true} \#{false}\")", '_', 'function, message string should not contain #{} formatting'
-      it_behaves_like 'a_detecting_cop', "#{decorator} \"a \#{true} \#{false}\"", '_', 'function, message string should not contain #{} formatting'
+      it_behaves_like 'a_detecting_cop', "#{decorator}(\"a \#{true} \#{false}\")", '_', error_message
+      it_behaves_like 'a_detecting_cop', "#{decorator} \"a \#{true} \#{false}\"", '_', error_message
     end
 
     context "#{decorator} decoration with interpolation on second string" do
-      it_behaves_like 'a_detecting_cop', "#{decorator}(\"a string\" + \"\#{true} \#{false}\")", '_', 'function, message string should not contain #{} formatting'
+      it_behaves_like 'a_detecting_cop', "#{decorator}(\"a string\" + \"\#{true} \#{false}\")", '_', error_message
     end
 
     context "#{decorator} decoration with a constant and then an interplation string" do
-      it_behaves_like 'a_detecting_cop', "#{decorator}(CONSTANT, \"a \#{true}\")", '_', 'function, message string should not contain #{} formatting'
+      it_behaves_like 'a_detecting_cop', "#{decorator}(CONSTANT, \"a \#{true}\")", '_', error_message
     end
   end
 end
